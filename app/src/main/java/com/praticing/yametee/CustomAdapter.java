580634c09e9a05cac2f86853296ed2d1164db3c2
KoniCm! <ezgamer489@gmail.com>
2023-05-04 15:15:41 +0800

    This is our project, hope it's work well

--- app/src/main/java/com/praticing/yametee/CustomAdapter.java
+++ app/src/main/java/com/praticing/yametee/CustomAdapter.java
@@ -1,6 +1,5 @@
 package com.praticing.yametee;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -52,7 +51,6 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         return new MyViewHolder(view);
     }
     //Tap . get position
-    @SuppressLint("RecyclerView")
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder,final int position)
     {
