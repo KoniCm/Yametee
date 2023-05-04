f13273b49f14b836dcd14067e3697492ef2fff51
KoniCm! <ezgamer489@gmail.com>
2023-05-04 17:19:22 +0800

    This is our project, hope it's work well

--- app/src/main/java/com/praticing/yametee/MainActivity.java
+++ app/src/main/java/com/praticing/yametee/MainActivity.java
@@ -1,11 +1,14 @@
 package com.praticing.yametee;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
